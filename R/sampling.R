b068e8cacf6bef79ac587ac26781cfcdfd067eb8
Ettie Unwin <h.unwin@imperial.ac.uk>
2021-10-28 06:01:18 +0100

    Update vignetts

--- R/sampling.R
+++ R/sampling.R
@@ -3,8 +3,10 @@
 #' @param n_iter Number of iterations
 #' @param r1 Ratio of cases not under surveillance (but contact traced) versus the cases under surveillance
 #' @param r2 Ratio of de novo cases verses detected cases under surveillance
-#' @param xmin Minimum value of uniform parameter scaling
-#' @param xmax Maximum value of uniform parameter scaling
+#' @param xmin_pi Minimum value of uniform parameter scaling
+#' @param xmax_pi Maximum value of uniform parameter scaling
+#' @param xmin_2 Minimum value of uniform parameter scaling
+#' @param xmax_2 Maximum value of uniform parameter scaling
 #' @param alpha Flag for if want to sample alpha or phi
 #'
 #' @return List of parameter chains
@@ -12,7 +14,7 @@
 #'
 #' @examples
 #' sample_chains(10, 0.5, 3)
-sample_chains <- function(n_iter, r1, r2, xmin = 0, xmax = 1, alpha = FALSE){
+sample_chains <- function(n_iter, r1, r2, xmin_pi = 0, xmax_pi = 1, xmin_2 = 0, xmax_2 = 1, alpha = FALSE){
   # Initialise chains
   mu_chain <- NULL
   pi_chain <- NULL
@@ -23,9 +25,9 @@ sample_chains <- function(n_iter, r1, r2, xmin = 0, xmax = 1, alpha = FALSE){
   for(i in 1:n_iter)
   {
     # Sample  pi and phi
-    pi <- stats::runif(n = 1, min = xmin, max = xmax)
+    pi <- stats::runif(n = 1, min = xmin_pi, max = xmax_pi)
     if (alpha == FALSE){
-      phi <- stats::runif(n = 1, min = xmin, max = xmax)
+      phi <- stats::runif(n = 1, min = xmin_2, max = xmax_2)
       # Compute alpha and mu
       sol <- compute_alpha_mu1(pi = pi, phi = phi, r1 = r1, r2 = r2)
       # If solution isn't na - add point to chain
@@ -36,7 +38,7 @@ sample_chains <- function(n_iter, r1, r2, xmin = 0, xmax = 1, alpha = FALSE){
         alpha_chain <- c(alpha_chain, sol$alpha)
       }
     } else {
-      alpha <- stats::runif(n = 1, min = xmin, max = xmax)
+      alpha <- stats::runif(n = 1, min = xmin_2, max = xmax_2)
       # Compute alpha and mu
       sol <- compute_phi_mu1(pi = pi, alpha = alpha, r1 = r1, r2 = r2)
       # If solution isn't na - add point to chain
